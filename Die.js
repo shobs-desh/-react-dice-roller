41f0e4d23820fa08eebc76a3c2681d0b4a1a8541
shobs-desh <mshobanamadurai@gmail.com>
2024-08-31 08:51:39 +0530

    react =dice roller
    react =dice roller

---
+++ Die.js
@@ -0,0 +1,20 @@
+ 
+import React, {Component} from 'react'
+import './Die.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class Die extends Component{
+render(){
+    const {face, rolling} = this.props
+    
+    return (
+            <div>
+                <FontAwesomeIcon icon={['fas', `fa-dice-${face}`]}
+                className={`Die 
+                ${rolling && 'Die-shaking'}`}/>
+            </div >
+        )
+}
+}
+
+export default Die
